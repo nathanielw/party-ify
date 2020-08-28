5af86bfab0e2421e051a3d98530ea463990886aa
Nathaniel Watson <nate@nw.nz>
2020-08-28 23:18:06 +1000

    Updates

---
+++ precache-manifest.5537865ce24eef0ee11038be9b42e89b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51cb75c89fe891f1cdbb2b560c68fa7",
+    "url": "/party-ify/index.html"
+  },
+  {
+    "revision": "ff047cb9ab09d7fd88d4",
+    "url": "/party-ify/static/css/main.708c89af.chunk.css"
+  },
+  {
+    "revision": "ef293881d43723b745dc",
+    "url": "/party-ify/static/js/2.49ba420b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/party-ify/static/js/2.49ba420b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff047cb9ab09d7fd88d4",
+    "url": "/party-ify/static/js/main.8417afea.chunk.js"
+  },
+  {
+    "revision": "11f01bbe790e1f45e7b2",
+    "url": "/party-ify/static/js/runtime-main.5ed28f68.js"
+  }
+]);
